8ff9e6f0df550dbaad40efdda02c5418e4176948
damsa.aatayde <damsa.aatayde@PC0DSASB-LAP>
2017-09-19 08:29:45 -0500

    Add project files.

---
+++ AngularFromScrach/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularFromScrach.MvcApplication" Language="C#" %>
---
+++ AngularFromScrach/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularFromScrach.Startup))]
+namespace AngularFromScrach
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
